3ee6d2d04f88c0f2629131022d9839620cb09b41
Vi <98347065+cyclikya@users.noreply.github.com>
2025-11-03 18:57:46 +0300

    upd

--- Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c5c3f2e3a77459c6889960d6ad2b7779b74eebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0e99b5279c2eb2cea52c616704fccd92ce55ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
